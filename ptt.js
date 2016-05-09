59b19edfa4e18c21bbe97b41e9f809c5afaa8108
es <es@evsfy.com>
2016-05-09 11:25:25 +0800

    .

--- ptt.js
+++ ptt.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-// var article = 'https://www.ptt.cc/bbs/Tainan/M.1388172150.A.860.html';
+var article = 'https://www.ptt.cc/bbs/Tainan/M.1388172150.A.860.html';
 
 var getLinkInContent = function(datadict) {
     content = datadict.content;
@@ -16,33 +16,35 @@ var getLinkInContent = function(datadict) {
     return datadict;
 };
 
-var runFetch = function(article) {
-    request(article, function(error, response, body) {
-        if(error) {
-            console.log("Error: " + error);
-        }
-        console.log("Status code: " + response.statusCode);
+var runFetch = request(article, function(error, response, body) {
+    if(error) {
+        console.log("Error: " + error);
+    }
+    console.log("Status code: " + response.statusCode);
 
-        var $ = cheerio.load(body);
-        var data = [];
-        $('#main-content > div.push').each(function() {
-            var datadict = {};
-            var tag = $(this).find('span.push-tag').text().trim();
-            var content = $(this).find('span.push-content').text().trim();
-            var userid = $(this).find('span.push-userid').text().trim();
-            var picture = [];
-            picture.push($(this).next('div.richcontent').find('img').attr('src'));
-            datadict = {
-                'tag': tag,
-                'content': content,
-                'userid': userid,
-                'picture': picture,
-            };
-            datadict = getLinkInContent(datadict);
-            data.push(datadict);
-        });
-        return 0;
+    var $ = cheerio.load(body);
+    var data = [];
+    $('#main-content > div.push').each(function() {
+        var datadict = {};
+        var tag = $(this).find('span.push-tag').text().trim();
+        var content = $(this).find('span.push-content').text().trim();
+        var userid = $(this).find('span.push-userid').text().trim();
+        var picture = [];
+        picture.push($(this).next('div.richcontent').find('img').attr('src'));
+        datadict = {
+            'tag': tag,
+            'content': content,
+            'userid': userid,
+            'picture': picture,
+        };
+        datadict = getLinkInContent(datadict);
+        data.push(datadict);
     });
+    return article;
+});
+
+var resultWrap = function (datadict) {
+    return datadict;
 };
 
 module.exports = runFetch;
--- test2.js
+++ test2.js
@@ -1,4 +1,5 @@
-var pttcrawl = require('./ptt.js');
+var pttcrawl2 = require('./ptt2.js');
 var article = 'https://www.ptt.cc/bbs/Tainan/M.1388172150.A.860.html';
 
-console.log(pttcrawl(article));
+console.log(pttcrawl2);
+console.log(Object.getOwnPropertyNames(pttcrawl2));

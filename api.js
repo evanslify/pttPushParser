3330401c69b8b6d3ef2858decf2854bde29ea073
es <es@evsfy.com>
2016-05-10 00:14:51 +0800

    remove regex image matching; add CORS

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ var startCrawl = require('./ptt.js');
 
 exports.goCrawl = function(req, res) {
     var callback = function (data) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.send(data);
     };
     var url = 'https://www.ptt.cc/bbs/tainan/M.1388172150.A.860.html';
--- ptt.js
+++ ptt.js
@@ -3,18 +3,18 @@ var cheerio = require('cheerio');
 var schema = require('./models.js');
 
 
-var getLinkInContent = function(item) {
-    var content = item.content;
-    var picture = item.picture;
-    var newPicture = [];
-    var urlRegex = /(https?:\/\/[^\s]+)/g;
-    if (content.match(urlRegex)) {
-        newPicture.push.apply(newPicture, picture);
-        newPicture.push.apply(newPicture, content.match(urlRegex));
-        item.picture = newPicture;
-    }
-    return item;
-};
+// var getLinkInContent = function(item) {
+    // var content = item.content;
+    // var picture = item.picture;
+    // var newPicture = [];
+    // var urlRegex = /(https?:\/\/[^\s]+)/g;
+    // if (content.match(urlRegex)) {
+        // newPicture.push.apply(newPicture, picture);
+        // newPicture.push.apply(newPicture, content.match(urlRegex));
+        // item.picture = newPicture;
+    // }
+    // return item;
+// };
 
 
 function addToDB(item, collection) {
@@ -37,8 +37,9 @@ function runFetch (article, callback) {
             item.content = $(this).find('span.push-content').text().trim();
             item.userid = $(this).find('span.push-userid').text().trim();
             item.picture = [];
-            item.picture.push($(this).next('div.richcontent').find('img').attr('src'));
-            item = getLinkInContent(item);
+            // item.picture.push($(this).next('div.richcontent').find('img').attr('src'));
+            item.picture.push($(this).nextUntil('div.push').find('img').attr('src'));
+            // item = getLinkInContent(item);
             addToDB(item, collection);
         });
         callback(collection);

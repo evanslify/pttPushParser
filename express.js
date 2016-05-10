ce4439ff128e99a1311e043420d86cc42e38b44c
es <es@evsfy.com>
2016-05-10 16:41:16 +0800

    migrate static file serving to express

--- express.js
+++ express.js
@@ -1,11 +1,17 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var api = require('./api.js');
+var path = require('path');
 
-app.get('/', function(req, res) {
+app.use(express.static(
+    path.join(__dirname, '/web/'))
+);
+
+app.get('/api/', function(req, res) {
     console.log('Request received.');
     api.goCrawl(req, res);
 });
 
-app.listen(3000, function () {
+app.listen(8000, function () {
   console.log('i am listening on port 3000!');
 });
